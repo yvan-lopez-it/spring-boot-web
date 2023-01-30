99c4daccfb1f673fb95bc1a46213c8874e0efd84
Yvan Lopez <yvan.lopez.it@gmail.com>
2023-01-29 21:34:38 -0500

    start upload

---
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package com.bolsadeideas.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/app")
+public class IndexController {
+
+	@GetMapping({ "/index", "/", "", "/home" })
+	public String index(Model model) {
+		model.addAttribute("titulo", "Hola Spring Framework");
+		return "index";
+	}
+
+}
