95ec1554da94dd0e0a777a607e1aa777ddd6658e
Yvan Lopez <yvan.lopez.it@gmail.com>
2023-05-15 23:22:13 -0500

    Clase 30.

---
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
@@ -0,0 +1,46 @@
+package com.bolsadeideas.springboot.web.app.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/params")
+public class EjemploParamsController {
+
+    @GetMapping("/")
+    public String index() {
+        return "params/index";
+    }
+
+    @GetMapping("/string")
+    public String param(@RequestParam(name = "texto", required = false, defaultValue = "Algun valor") String texto, Model model) {
+        model.addAttribute("resultado", "El texto enviado es: ".concat(texto));
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params")
+    public String param(@RequestParam String saludo, @RequestParam Integer numero, Model model) {
+        model.addAttribute("resultado", "El saludo enviado es: '" + saludo + "' y el número es: '" + numero + "' ");
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params-request")
+    public String param(HttpServletRequest request, Model model) {
+        String saludo = request.getParameter("saludo");
+        int numero;
+
+        try {
+            numero = Integer.parseInt(request.getParameter("numero"));
+        } catch (NumberFormatException e) {
+            numero = 0;
+        }
+
+        model.addAttribute("resultado", "El saludo enviado es: '" + saludo + "' y el número es: '" + numero + "' ");
+        return "params/ver";
+    }
+
+}
--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,18 +1,64 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bolsadeideas.springboot.web.app.models.Usuario;
 
 @Controller
 @RequestMapping("/app")
 public class IndexController {
 
-	@GetMapping({ "/index", "/", "", "/home" })
-	public String index(Model model) {
-		model.addAttribute("titulo", "Hola Spring Framework");
-		return "index";
-	}
+    @Value("${texto.indexcontroller.index.titulo}")
+    private String textoIndex;
+
+    @Value("${texto.indexcontroller.perfil.titulo}")
+    private String textoPerfil;
+
+    @Value("${texto.indexcontroller.listar.titulo}")
+    private String textoListar;
+
+    @GetMapping({"/index", "/", "", "/home"})
+    public String index(Model model) {
+        model.addAttribute("titulo", textoIndex);
+        return "index";
+    }
+
+    @RequestMapping("/perfil")
+    public String perfil(Model model) {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Yvan");
+        usuario.setApellido("Lopez");
+        usuario.setEmail("yvan@mail.com");
+
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", textoPerfil.concat(usuario.getNombre()));
+
+        return "perfil";
+
+    }
+
+    @RequestMapping("/listar")
+    public String listar(Model model) {
+        model.addAttribute("titulo", textoListar);
+        return "listar";
+    }
+
+    @ModelAttribute("usuarios")
+    public List<Usuario> poblarUsuarios() {
+        return Arrays.asList(
+            new Usuario("yvan", "lopez", "yvan@mail.com"),
+            new Usuario("benji", "lopez", "benji@mail.com"),
+            new Usuario("fausto", "lopez", "fausto@mail.com"),
+            new Usuario("maritza", "lopez", "maritza@mail.com")
+        );
+    }
 
 }
